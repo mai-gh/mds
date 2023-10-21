0333a36ab53b3a522d6920bd1e2c4ceda208a907
Mai <jea989@gmail.com>
2023-10-20 23:54:57 -0400

    fixed endless loading

--- mds.rb
+++ mds.rb
@@ -11,15 +11,13 @@ work_dir = file.split("/")[0...-1].join("/")+"/"
 server = TCPServer.new(port)
 
 def watch_file(f)
-  puts "#{Time.now}: Reload"
   notifier = INotify::Notifier.new
   notifier.watch(f, :modify) {sendreload}
   notifier.process
 end
 
 def sendpage()
-  rc = Redcarpet::Markdown.new(
-    Redcarpet::Render::HTML,
+  rc = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
     tables: true,
     fenced_code_blocks: true,
     strikethrough: true,
@@ -50,9 +48,23 @@ def sendpage()
   <body>
     <article class='markdown-body'>#{md}</article>
     <script>
-      const socket = new WebSocket('ws://localhost:8000');
-      socket.addEventListener('message', e => {window.location.reload();});
+      const socket = new WebSocket('ws://127.0.0.1:8000');
+      socket.addEventListener('message', e => {
+        if (e.data == 'RELOAD') {
+          window.location.reload();
+        }
+      });
     </script>
+  </body>
+</html>
+"
+  thtml = "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <div>HHHHHHHHHHHHH</div>
+    <img src=example.jpg></img>
   </body>
 </html>
 "
@@ -60,19 +72,18 @@ def sendpage()
   $session.print("Content-Type: text/html\r\n")
   $session.print("\r\n")
   $session.print(html)
+  $session.close
 end
 
 def sendreload()
   response = "RELOAD"
-  #STDERR.puts "Sending response: #{ response.inspect }"
   output = [0b10000001, response.size, response]
   $session.write output.pack("CCA#{ response.size }")
+  puts "Sent: RELOAD"
 end
 
 def handshake(websocket_key)
-  #STDERR.puts "Websocket handshake detected with key: #{ websocket_key }"
   response_key = Digest::SHA1.base64digest([websocket_key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"].join)
-  #STDERR.puts "Responding to handshake with key: #{ response_key }"
   wsresp = ""
   wsresp += "HTTP/1.1 101 Switching Protocols\r\n"
   wsresp += "Upgrade: websocket\r\n"
@@ -80,6 +91,19 @@ def handshake(websocket_key)
   wsresp += "Sec-WebSocket-Accept: #{response_key}\r\n"
   wsresp += "\r\n"
   $session.write wsresp
+  STDERR.puts "Handshake completed."
+end
+
+def sendfile(fp)
+  ext = fp.split(".")[-1]
+  if (File.file?(fp))
+    s = File.open(fp, 'rb') { |io| io.read }
+    $session.print("HTTP/1.1 200\r\n")
+    $session.print("Content-Type: image/#{ext}\r\n")
+    $session.print("\r\n")
+    $session.write(s)
+    $session.close
+  end      
 end
 
 puts("#{Time.now}: Watching for modifications to #{file}")
@@ -90,22 +114,12 @@ while $session = server.accept()
     http_request += line
   end
 
-  #STDERR.puts http_request
-
   if (matches = http_request.match(/^Sec-WebSocket-Key: (\S+)/))
     handshake matches[1]
     Thread.new { watch_file file }
   elsif (http_request.split(/\r\n/)[0] == "GET / HTTP/1.1")
     sendpage
   else
-    fp = work_dir + http_request.split(/\r\n/)[0].split(" ")[1].gsub(/\//, "")
-    ext = fp.split(".")[-1]
-    if (File.file?(fp))
-      s = File.open(fp, 'rb') { |io| io.read }
-      $session.print("HTTP/1.1 200\r\n")
-      $session.print("Content-Type: image/#{ext}\r\n")
-      $session.print("\r\n")
-      $session.write(s)
-    end      
+    sendfile work_dir + http_request.split(/\r\n/)[0].split(" ")[1].gsub(/\//, "")
   end
 end
